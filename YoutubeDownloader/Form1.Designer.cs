29ab475f8b5e4b64913a9138cf95ee079b37b3c9
GuiChambelPt <guichambel08@gmail.com>
2024-12-27 02:52:15 +0000

    I used claude to do this cuz is so fucking confusing

--- YoutubeDownloader/Form1.Designer.cs
+++ YoutubeDownloader/Form1.Designer.cs
@@ -28,41 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttondownload = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // textBox2
             // 
-            this.button1.Location = new System.Drawing.Point(282, 229);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(255, 117);
-            this.button1.TabIndex = 0;
-            this.button1.Text = " Download";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox2.Location = new System.Drawing.Point(219, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(434, 20);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // richTextBox1
+            // buttondownload
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(219, 165);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(390, 31);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.buttondownload.Location = new System.Drawing.Point(319, 191);
+            this.buttondownload.Name = "buttondownload";
+            this.buttondownload.Size = new System.Drawing.Size(202, 60);
+            this.buttondownload.TabIndex = 1;
+            this.buttondownload.Text = "download";
+            this.buttondownload.UseVisualStyleBackColor = true;
+            this.buttondownload.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.BackgroundImage = global::YoutubeDownloader.Properties.Resources.images;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(897, 434);
+            this.Controls.Add(this.buttondownload);
+            this.Controls.Add(this.textBox2);
             this.Name = "Form1";
-            this.Text = "YT Downloader | github.com/GuiChambelPt";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +65,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttondownload;
     }
 }
 
--- YoutubeDownloader/Form1.cs
+++ YoutubeDownloader/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
 
 namespace YoutubeDownloader
 {
@@ -17,17 +20,51 @@ namespace YoutubeDownloader
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            MessageBox.Show("Nos vai descer para BC");
-            string link = richTextBox1.Text;
-            Console.WriteLine(link);
 
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        private async void button3_Click(object sender, EventArgs e)
         {
+            try
+            {
+                string link = textBox2.Text;
+                Console.WriteLine(link);
 
+                // Create FolderBrowserDialog
+                using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+                {
+                    folderDialog.Description = "Select Download Location";
+
+                    // Show the dialog and get result
+                    if (folderDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        // Get the selected path
+                        string downloadPath = folderDialog.SelectedPath;
+
+                        var youtube = new YoutubeClient();
+                        var videoUrl = link;  // Using the link from richTextBox1
+                        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoUrl);
+                        var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+                        var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+
+                        // Combine the selected path with the filename
+                        string fullPath = Path.Combine(downloadPath, $"video.{streamInfo.Container}");
+
+                        // Download to the selected location
+                        await youtube.Videos.Streams.DownloadAsync(streamInfo, fullPath);
+
+                        MessageBox.Show("Download completed successfully!", "Success",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
