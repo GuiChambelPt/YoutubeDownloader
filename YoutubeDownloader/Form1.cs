cf6925dd2642fad0ab2599d01c8ce08ca5eef494
aluno <aluno@DESKTOP-ALACFTG>
2024-12-27 02:20:07 +0000

    Adicionar arquivos de projeto.

---
+++ YoutubeDownloader/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoutubeDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nos vai descer para BC");
+            string link = richTextBox1.Text;
+            Console.WriteLine(link);
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
